3d3115b6db58443e5231dc32582bfdc32617138d
Jioponda <jioponda@outlook.fr>
2022-10-06 17:34:58 +0200

    test barre de recherche étape 3

--- JS/rechercheDeRecettes.js
+++ JS/rechercheDeRecettes.js
@@ -1,10 +1,11 @@
 /** Recherchez une recette dans la barre principale */ 
 const searchBar = document.querySelector("#searchbar");
-const divCard = document.querySelectorAll(".div-card");
 const cardTitle = document.querySelectorAll(".card-title");
-searchBar.addEventListener('keyup', function() {
 const searchValue = searchBar.value;
-if (cardTitle.textContent !== searchValue) {
-    divCard.style.display = "none";
-};
+searchBar.addEventListener('keyup', function() {
+    if (cardTitle.textContent !== searchValue) {
+        const divCard = document.querySelector(".div-card");
+        divCard.style.display = "none";
+        console.log(searchValue);
+    };
 })
